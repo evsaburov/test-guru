483bea8230fad441d0a40fe8840b30db6c3b39c6
Evgeniy Saburov <evs.zlat@gmail.com>
2021-02-17 21:43:54 +0500

    add default for ansewr and test

--- db/migrate/20210217155300_create_answers.rb
+++ db/migrate/20210217155300_create_answers.rb
@@ -2,8 +2,8 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
       t.text :body, null: false
+      t.text :correct, null: false, default: 'empty'
       t.integer :question_id, null: false
-
       t.timestamps
     end
   end
