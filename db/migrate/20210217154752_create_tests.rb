b44fe08af65b61c325b35c7ded62f23e00c6cf74
Evgeniy Saburov <evs.zlat@gmail.com>
2021-02-19 21:20:04 +0500

    fix migrations

--- db/migrate/20210217154752_create_tests.rb
+++ db/migrate/20210217154752_create_tests.rb
@@ -4,7 +4,8 @@ class CreateTests < ActiveRecord::Migration[6.1]
       t.text :title, null: false
       t.integer :level, null: false, default: 0
       t.references :categoriy, null: false, foreign_key: true
-
+      t.reference :user, null: false, foreign_key: true
+      
       t.timestamps
     end
   end
--- db/migrate/20210217155300_create_answers.rb
+++ db/migrate/20210217155300_create_answers.rb
@@ -2,9 +2,8 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
       t.text :body, null: false
-      t.text :correct, null: false, default: 'empty'
+      t.boolean :correct, null: false, default: false
       t.references :question, null: false, foreign_key: true
-      t.reference :user, null: false, foreign_key: true
 
       t.timestamps
     end
