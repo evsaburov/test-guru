68098c83d4beb22b2e779ed6eb36c977f97c14d8
Evgeniy Saburov <evs.zlat@gmail.com>
2021-02-20 17:09:30 +0500

    fix migrations

--- db/migrate/20210217154752_create_tests.rb
+++ db/migrate/20210217154752_create_tests.rb
@@ -4,7 +4,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
       t.text :title, null: false
       t.integer :level, null: false, default: 0
       t.references :categoriy, null: false, foreign_key: true
-      t.reference :user, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
